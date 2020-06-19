4a13869ac4446d51644396bb284c6adf2a0a61e1
tongplw <bangkok.tong@gmail.com>
2020-06-19 12:07:14 +0700

    add requirements.txt

--- settings.py
+++ settings.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-FUND_FACTSHEET_KEY = os.getenv("FUND_FACTSHEET_KEY")
-FUND_DAILY_INFO_KEY = os.getenv("FUND_DAILY_INFO_KEY")
+FUND_FACTSHEET_KEY = eval(os.getenv("FUND_FACTSHEET_KEY"))
+FUND_DAILY_INFO_KEY = eval(os.getenv("FUND_DAILY_INFO_KEY"))
